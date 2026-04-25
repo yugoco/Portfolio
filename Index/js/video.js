3281678d7a8f76af856512db7b95a5a3030ddb92
yugoco <hugo.yugoco@gmail.com>
2026-04-25 12:31:14 -0400

    Add files via upload

--- Index/js/video.js
+++ Index/js/video.js
@@ -1,3 +1,5 @@
+
+//Demo vfx
 const modalEl = document.getElementById('videoModal');
 const iframe = modalEl.querySelector('iframe');
 const src = iframe.getAttribute('src');
@@ -10,6 +12,7 @@ modalEl.addEventListener('show.bs.modal', () => {
     iframe.setAttribute('src', src); // Recharge la vidéo quand la modal s'ouvre
 });
 
+//Videoclip animation
 const modalEl1 = document.getElementById('clipModal');
 const iframe1 = modalEl1.querySelector('iframe');
 const src1 = iframe1.getAttribute('src');
@@ -20,4 +23,43 @@ modalEl1.addEventListener('hide.bs.modal', () => {
 
 modalEl1.addEventListener('show.bs.modal', () => {
     iframe1.setAttribute('src', src1); // Recharge la vidéo quand la modal s'ouvre
+});
+
+//Mike-a-Rona
+const modalEl2 = document.getElementById('mikearonaModal');
+const iframe2 = modalEl2.querySelector('iframe');
+const src2 = iframe2.getAttribute('src');
+
+modalEl2.addEventListener('hide.bs.modal', () => {
+    iframe2.setAttribute('src', ''); // Vide le src → stoppe la vidéo
+});
+
+modalEl2.addEventListener('show.bs.modal', () => {
+    iframe2.setAttribute('src', src2); // Recharge la vidéo quand la modal s'ouvre
+});
+
+//Rona Laval
+const modalEl3 = document.getElementById('ronalavalModal');
+const iframe3 = modalEl3.querySelector('iframe');
+const src3 = iframe3.getAttribute('src');
+
+modalEl3.addEventListener('hide.bs.modal', () => {
+    iframe3.setAttribute('src', ''); // Vide le src → stoppe la vidéo
+});
+
+modalEl3.addEventListener('show.bs.modal', () => {
+    iframe3.setAttribute('src', src3); // Recharge la vidéo quand la modal s'ouvre
+});
+
+//MESS
+const modalEl4 = document.getElementById('messModal');
+const iframe4 = modalEl4.querySelector('iframe');
+const src4 = iframe4.getAttribute('src');
+
+modalEl4.addEventListener('hide.bs.modal', () => {
+    iframe4.setAttribute('src', ''); // Vide le src → stoppe la vidéo
+});
+
+modalEl4.addEventListener('show.bs.modal', () => {
+    iframe4.setAttribute('src', src4); // Recharge la vidéo quand la modal s'ouvre
 });
